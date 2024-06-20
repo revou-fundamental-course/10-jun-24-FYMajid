f4a9177258e1b7de1e0ad94c75d2bc17a2db6fe1
Fathi Yakan Majid <fathiyakanmajid81@gmail.com>
2024-06-21 01:07:27 +0700

    membuat function

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function inputKonversi() {
+  console.log("hallo world");
+}
+
+const inputReset = () => {
+  console.log("hallo world");
+};
+
+const inputReverse = () => {
+  console.log("hallo world");
+};
